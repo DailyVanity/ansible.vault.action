4368f85e497711a42eda8fb06a06fe8cf158e441
Chris Sim <monkeymon@gmail.com>
2020-05-10 13:05:56 +0800

    change script location

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,6 +28,6 @@ else
   echo ansible-vault decrypt ${VAULTFILE} ${FILES}
   ansible-vault decrypt ${VAULTFILE} ${FILES}
   chmod -Rf 777 ${FILES}
-  node setSecret.js ${FILES}
+  node /setSecret.js ${FILES}
 fi
 
